3b0819bf20c52605f33908b2888b85878b702630
James Newman <jdnewman85@gmail.com>
2021-04-25 20:58:13 -0600

    Get uf8 string packets working and sent through an mpsc channel

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ use tokio_util::udp::UdpFramed;
 
 use futures::stream::StreamExt;
 
+use tokio::sync::mpsc;
+use bytes::Buf;
+
 /*
 use gpio_cdev::{Chip, AsyncLineEventHandle, LineRequestFlags, EventRequestFlags};
 
@@ -33,13 +36,21 @@ impl Decoder for Packet {
     type Item = FeedPacket;
     type Error = std::io::Error;
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        let s = str::from_utf8(&src).unwrap();
-        Ok(Some(FeedPacket{
-            data: String::from(s)
-        }))
+        let remaining = src.remaining();
+        let source_bytes = src.copy_to_bytes(remaining);
+        src.clear();
+
+        if remaining > 0 {
+            let s = String::from_utf8(source_bytes.to_vec()).unwrap();
+            Ok(Some(FeedPacket{
+                data: s
+            }))
+        } else {
+            Ok(None)
+        }
     }
 }
-const ADDRESS: &str = "0.0.0.0:6000";
+const ADDRESS: &str = "0.0.0.0:7000";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -47,6 +58,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
   let socket = UdpSocket::bind(ADDRESS.parse::<SocketAddr>().unwrap()).await?;
   let mut framed_socket = UdpFramed::new(socket, Packet{});
+
+  let (tx, mut rx) = mpsc::channel(10);
 
   /*
   // Feed service
@@ -69,8 +82,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
   )?;
   */
 
+  tokio::spawn(async move {
+      while let Some(feed_packet) = rx.recv().await {
+          dbg!(feed_packet);
+      }
+  });
+
   while let Some(Ok(feed_packet)) = framed_socket.next().await {
-      dbg!("FeedPacket: {:?}", feed_packet);
+      dbg!(&feed_packet);
+      tx.send(feed_packet).await.unwrap();
   }
 
   Ok(())
