2bb3c629462a5eed049600abdddfd42a53be9e10
James Newman <jdnewman85@gmail.com>
2021-05-02 18:25:52 -0600

    First attempt at async/await

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,20 @@ use futures::stream::StreamExt;
 use tokio::sync::mpsc;
 use bytes::Buf;
 
-/*
 use gpio_cdev::{Chip, AsyncLineEventHandle, LineRequestFlags, EventRequestFlags};
+
+use gpio_cdev::{EventType};
+
+use tokio::time::{sleep, Duration};
 
 const PIN_IR:u32 = 16;
 const PIN_SWITCH:u32 = 26;
 
-const ON: u8 = 1;
-const OFF: u8 = 0;
+const MOTOR_ON: u8 = 1;
+const MOTOR_OFF: u8 = 0;
 const PIN_MOTOR:u32 = 4;
-*/
+
+const DELAY_DEBOUNCE:u64 = 200;
 
 #[derive(Debug)]
 struct FeedPacket {
@@ -62,7 +66,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
   let (tx, mut rx) = mpsc::unbounded_channel();
 
-  /*
   // Feed service
   let mut chip = Chip::new("/dev/gpiochip0")?;
   let switch_line = chip.get_line(PIN_SWITCH)?;
@@ -81,11 +84,42 @@ async fn main() -> Result<(), Box<dyn Error>> {
       "ir"
     )?
   )?;
-  */
+
+  let motor = chip
+      .get_line(PIN_MOTOR)?
+      .request(LineRequestFlags::OUTPUT, 0, "motor")?;
 
   tokio::spawn(async move {
       while let Some(feed_packet) = rx.recv().await {
-          dbg!(feed_packet);
+          dbg!(&feed_packet);
+          // Wait for ir off
+          while let Some(Ok(ir_event)) = ir_stream.next().await {
+              if ir_event.event_type() == EventType::FallingEdge {
+                  break;
+              }
+          }
+
+          // Motor on
+          motor.set_value(MOTOR_ON).expect("Unable to turn motor on!");
+
+          // Wait for switch on
+          while let Some(Ok(switch_event)) = switch_stream.next().await {
+              if switch_event.event_type() == EventType::RisingEdge {
+                  break;
+              }
+          }
+          // Wait for debounce
+          sleep(Duration::from_millis(DELAY_DEBOUNCE)).await;
+
+          // Wait for switch off
+          while let Some(Ok(switch_event)) = switch_stream.next().await {
+              if switch_event.event_type() == EventType::FallingEdge {
+                  break;
+              }
+          }
+
+          //Motor off
+          motor.set_value(MOTOR_OFF).expect("Unable to turn motor off!");
       }
   });
 
